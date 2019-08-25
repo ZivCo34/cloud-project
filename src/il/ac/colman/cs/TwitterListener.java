eed305b5a380383b50c872f2c199c0a409e4debd
Shirapika <shirapika51@gmail.com>
2019-08-25 14:30:02 +0300

    sqs

--- src/il/ac/colman/cs/TwitterListener.java
+++ src/il/ac/colman/cs/TwitterListener.java
@@ -1,5 +1,7 @@
 package il.ac.colman.cs;
 
+import com.amazonaws.services.sqs.*;
+
 import il.ac.colman.cs.util.LinkExtractor;
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
@@ -17,12 +19,38 @@ public class TwitterListener {
     // Create our Twitter stream
     TwitterStreamFactory tf = new TwitterStreamFactory(cb.build());
     TwitterStream twitterStream = tf.getInstance();
+    
+	AmazonSQS client = AmazonSQSClientBuilder.defaultClient();
 
-
+    StatusListener listener = new StatusListener(){
+    	
+        public void onStatus(Status status) {
+            client.sendMessage("https://sqs.us-east-1.amazonaws.com/042445528747/TweetQueue",status.getText());
+        }
+        public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
+        public void onTrackLimitationNotice(int numberOfLimitedStatuses) {}
+        public void onException(Exception ex) {
+            ex.printStackTrace();
+        }
+		@Override
+		public void onScrubGeo(long arg0, long arg1) {
+			// TODO Auto-generated method stub
+			
+		}
+		@Override
+		public void onStallWarning(StallWarning arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+    };
+    
+    twitterStream.addListener(listener);
     /*
       This is where we should start fetching the tweets using the Streaming API
       See Example 9 on this page: http://twitter4j.org/en/code-examples.html#streaming
     */
     twitterStream.filter(System.getProperty("https://"));
+    
+    twitterStream.sample();
   }
 }
