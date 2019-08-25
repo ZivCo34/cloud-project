a184b7ea15d4a7f4224e64e859d902f954f9ddfd
Shirapika <shirapika51@gmail.com>
2019-08-25 14:36:06 +0300

    sqs get

--- src/il/ac/colman/cs/LinkListener.java
+++ src/il/ac/colman/cs/LinkListener.java
@@ -1,6 +1,11 @@
 package il.ac.colman.cs;
 
 import java.sql.SQLException;
+import java.util.List;
+
+import com.amazonaws.services.sqs.*;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageResult;
 
 import il.ac.colman.cs.util.DataStorage;
 import il.ac.colman.cs.util.LinkExtractor;
@@ -14,7 +19,22 @@ public class LinkListener {
     LinkExtractor linkExtractor = new LinkExtractor();
 
     // Listen to SQS for arriving links
-    // ...dfd
+    AmazonSQS client = AmazonSQSClientBuilder.defaultClient();
+    
+    while (true) {
+    	ReceiveMessageResult result = client.receiveMessage("https://sqs.us-east-1.amazonaws.com/042445528747/TweetQueue");
+    	List<Message> messages = result.getMessages();
+    	if (messages.size() == 0) {
+    		try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {}
+    	}
+    	else {
+    		for (Message message : messages) {
+    			// Do something with message
+    		}
+    	}
+    }
 
     // Extract the link content
     // ...
